82edf1803ee9d1f97c4193393dcc8d808199693c
Anjali Choudhary <anjalichoudhary@Anjalis-Air.mshome.net>
2024-04-25 14:05:46 +0530

    Techdome_assessment

--- program2.js
+++ program2.js
@@ -1,6 +1,20 @@
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
+    nums = nums.filter(num => num > 0);
+
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+        if (index < nums.length && nums[index] > 0) {
+            nums[index] = -nums[index];
+        }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            return i + 1;
+        }
+    }
+    return nums.length + 1;
 
 }
 
